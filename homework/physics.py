e5ba9a0704ec0d1bc44be632c578cd82a8d347cb
MatthewKosloski <matthewkosloski@me.com>
2018-02-28 16:23:45 -0600

    remove unit tests from physics assignment

--- homework/physics.py
+++ homework/physics.py
@@ -20,26 +20,19 @@ def main():
 	Then, ask the user if they would like to go again.
 	'''
 
-	if not DEBUG:
+	print('\nYou will be prompted to enter in a time',
+	'in seconds, \nin which an object fell and '
+	'the mass, in kilograms,\nof the object. After, ' 
+	'you will be provided with the \nfollowing calculations: '
+	'distance, velocity, and kin-\netic energy.\n')
 
-		print('You will be prompted to enter in a time',
-		'in seconds, \nin which an object fell and '
-		'the mass, in kilograms,\nof the object. After, ' 
-		'you will be provided with the \nfollowing calculations: '
-		'distance, velocity, and kin-\netic energy. \n\nTo run '
-		'unit tests, edit the "DEBUG" global constant.\n\n')
+	another = 'y'
 
-		another = 'y'
+	while another.lower() == ANOTHER:
 
-		while another.lower() == ANOTHER:
+		drop_physics(get_time(), get_mass())
 
-			drop_physics(get_time(), get_mass())
-
-			another = input('Another? (y/n): ')
-
-	else:
-		print('Running unit tests...\n\n')
-		run_unit_tests()
+		another = input('Another? (y/n): ')
 
 def get_time():
 	'''Prompts the user to enter a time, in seconds,
@@ -209,139 +202,5 @@ def round_hundredths(num):
 
 	'''
 	return round(num, 2)
-
-def run_unit_tests():
-	'''
-		Rudimentary unit tests that compare
-		the expected result (value and datatype)
-		to the actual result.
-	'''
-	describe('get_falling_distance()', [{
-		'given': 'a float',
-		'should': 'return a float',
-		'expected': 17640.0,
-		'actual': get_falling_distance(time=60)
-	}])
-	describe('get_velocity()', [{
-		'given': 'a float and an int',
-		'should': 'return a float',
-		'expected': 83.33,
-		'actual': get_velocity(distance=5000, time=60)
-	}])
-	describe('get_kinetic_energy()', [{
-		'given': 'two floats',
-		'should': 'return a float',
-		'expected': 302400.0,
-		'actual': get_kinetic_energy(mass=42, velocity=120)
-	}])
-	describe('left_adjust_str()', [{
-		'given': 'a string',
-		'should': 'return a string',
-		'expected': 'Matthew                  ',
-		'actual': left_adjust_str(str='Matthew')
-	}])
-	describe('format_item()', [{
-		'given': 'two strings and a float',
-		'should': 'return a string',
-		'expected': 'Item                      123.00 Kilograms',
-		'actual': format_item(label='Item', value=123.00, unit='Kilograms')
-	}])
-	describe('round_hundredths()', [{
-		'given': 'a float',
-		'should': 'return a float',
-		'expected': 3.14,
-		'actual': round_hundredths(num=3.141519)
-	}])
-
-def describe(unit, tests):
-	'''Initiates tests on a function
-
-	Args:
-		unit (str): Title of the unit being tested
-		tests (list -> dict) A list of dictionaries
-	
-	Returns: void
-	'''
-
-	print(unit)
-	for test in tests:
-		unit_test(test)
-	print()
-
-def unit_test(unit):
-	'''Unit tests an individual test.
-	
-	Args:
-		unit (dict): A dictionary with the following
-		keys: given, should, expected, actual
-
-	Returns: void
-	'''
-
-	expected = unit['expected']
-	actual = unit['actual']
-
-	if does_return_expected(expected, actual):
-		print('  (OK) Given %s: should %s' % (
-			unit['given'], 
-			unit['should']))
-	else:
-		print('  (X) Expected "%s" (%s), but got "%s" (%s)' % (
-			expected, 
-			type_str(expected), 
-			actual, 
-			type_str(actual)))
-
-def does_return_expected(expected, actual):
-	'''Checks if expected returns the correct value and datatype.
-	
-	Args:
-		expected (any): Any datatype
-		actual (any): Any datatype
-	
-	Returns:
-		bool: If both types have same value and datatype
-	'''
-
-	return is_same_value(expected, actual) and is_same_type(expected, actual)
-
-def is_same_value(expected, actual):
-	'''Checks to see if value of expected is the
-	value of actual.
-
-	Args:
-		expected (any): Any datatype
-		actual (any): Any datatype
-	
-	Returns:
-		bool: If both types have same value
-	'''
-
-	return expected == actual
-
-def is_same_type(expected, actual):
-	'''Checks to see if datatype of expected is the
-	same as that of actual.
-
-	Args:
-		expected (any): Any datatype
-		actual (any): Any datatype
-	
-	Returns:
-		bool: If both types have same datatypes
-	'''
-
-	return type(expected) == type(actual)
-
-def type_str(val):
-	'''Returns the type of val in string form.
-
-	Args:
-		val (any): Any datatype
-
-	Returns:
-		str: The datatype of val (e.g., str, bool, int, float, etc.)
-	'''
-	return type(val).__name__
 
 main()
